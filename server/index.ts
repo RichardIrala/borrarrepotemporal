7d8c9767da084d99765a7a1eea7c57082f434d4a
Lucas <lucasmruiz05@gmail.com>
2022-06-01 20:39:35 -0300

    add @types/node

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,5 @@
 import * as express from "express";
-import * as cors from "cors";
 import * as path from "path";
-import { nanoid } from "nanoid";
 
 const app = express();
 app.use(express.json());
