9422c9d1973168b2140e2d207a5840c0589c88f6
Lucas <lucasmruiz05@gmail.com>
2022-06-01 20:22:58 -0300

    init project

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+import * as cors from "cors";
+import * as path from "path";
+import { nanoid } from "nanoid";
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/test", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(port, () => {
+  console.log("escuchando al puerto: ", port);
+});
+
+const relativeRoute = path.resolve(__dirname, "../dist");
+
+app.use(express.static(relativeRoute));
+app.get("*", (req, res) => {
+  res.sendFile(relativeRoute, +"/index.html");
+});
