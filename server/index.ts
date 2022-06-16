c61ee9a0a4c72a3562fe401f1e916d533e0e2a37
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-16 12:13:54 -0300

    Retocando el EndPoint /rooms 8

--- server/index.ts
+++ server/index.ts
@@ -106,32 +106,9 @@ app.post("/rooms", (req, res) => {
       if (doc.exists) {
         const idRandom = nanoid();
         const roomRef = rtdb.ref("/rooms/" + idRandom);
-        roomRef
-          .set({
-            player1: {
-              userName,
-              moveChoise: "none",
-              start: false,
-              online: true,
-            },
-          })
-          .then(() => {
-            const roomLongId = roomRef.key;
-            const roomId = 1000 + Math.floor(Math.random() * 999);
-            roomsColl
-              .doc(roomId.toString())
-              .set({
-                rtdbId: roomLongId,
-                player1: userName,
-                scorePlayer1: 0,
-                scorePlayer2: 0,
-              })
-              .then(() => {
-                res.status(200).json({
-                  id: roomId,
-                });
-              });
-          });
+        roomRef.set({
+          player1: "dsadsadsa",
+        });
       } else {
         res.status(401).json({
           message: "El usuario no existe.",
