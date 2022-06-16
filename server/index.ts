9c928996f006a89880029269dbbfd4825ab1fb26
Lucas <lucasmruiz05@gmail.com>
2022-06-16 09:53:22 -0300

    change static

--- server/index.ts
+++ server/index.ts
@@ -256,7 +256,7 @@ app.put("/rooms/:id/player/move", (req, res) => {
 
 const relativeRoute = path.resolve(__dirname, "../dist");
 
-app.use(express.static(relativeRoute));
+app.use(express.static("dist"));
 app.get("*", (req, res) => {
   res.sendFile(relativeRoute, +"/index.html");
 });
