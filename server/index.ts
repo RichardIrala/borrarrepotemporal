fa24fd41c19bc5b33b254f5837d817cce23d322f
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-16 11:53:04 -0300

    Retocando el EndPoint /rooms 2

--- server/index.ts
+++ server/index.ts
@@ -106,38 +106,42 @@ app.post("/rooms", (req, res) => {
       if (doc.exists) {
         const idRandom = nanoid();
         const roomRef = rtdb.ref("/rooms/" + idRandom);
-        roomRef.set({
-          player1: {
-            userName,
-            moveChoise: "none",
-            start: false,
-            online: true,
-          },
-          player2: {
-            userName: false,
-            moveChoise: "none",
-            start: false,
-            online: false,
-          },
-        });
+        roomRef
+          .set({
+            player1: {
+              userName,
+              moveChoise: "none",
+              start: false,
+              online: true,
+            },
+            player2: {
+              userName: false,
+              moveChoise: "none",
+              start: false,
+              online: false,
+            },
+          })
+          .then(() => {
+            res.json({ d: "a" });
+          });
+        // .then(() => {
+        //   const roomLongId = roomRef.key;
+        //   const roomId = 1000 + Math.floor(Math.random() * 999);
+        //   roomsColl
+        //     .doc(roomId.toString())
+        //     .set({
+        //       rtdbId: roomLongId,
+        //       player1: userName,
+        //       scorePlayer1: 0,
+        //       scorePlayer2: 0,
+        //     })
         //     .then(() => {
-        //       const roomLongId = roomRef.key;
-        //       const roomId = 1000 + Math.floor(Math.random() * 999);
-        //       roomsColl
-        //         .doc(roomId.toString())
-        //         .set({
-        //           rtdbId: roomLongId,
-        //           player1: userName,
-        //           scorePlayer1: 0,
-        //           scorePlayer2: 0,
-        //         })
-        //         .then(() => {
-        //           res.status(200).json({
-        //             id: roomId,
-        //           });
-        //         });
+        //       res.status(200).json({
+        //         id: roomId,
+        //       });
         //     });
-        // } else {
+        // });
+      } else {
         res.status(401).json({
           message: "El usuario no existe.",
         });
