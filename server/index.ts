044c694d741f5097b3edc9a31468d16ee4d54322
Richard Miguel Irala <richardirala47@hotmail.com>
2022-06-16 11:24:00 -0300

    Cambios nuevos4 quite los cors

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import { nanoid } from "nanoid";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 console.log(__dirname);
 const relativeRoute = path.resolve(__dirname, "../dist");
 const universalRoute = path.resolve(relativeRoute, "index.html");
